f64c0e1e8eb62d4d658db21cac8f6948b8d5ba4e
SpencerG <spencer.gonida@obf.ateneo.edu>
2021-08-05 22:48:48 +0800

    Updated UI for pages

--- Final_Project-Eat@Home/app/src/main/java/com/example/lab_3_realm_gonida_192287/MainActivity3.java
+++ Final_Project-Eat@Home/app/src/main/java/com/example/lab_3_realm_gonida_192287/MainActivity3.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.ViewById;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+public class MainActivity3 extends AppCompatActivity {
+    @Click(R.id.viewOrdersMM){
+
     }
 }
