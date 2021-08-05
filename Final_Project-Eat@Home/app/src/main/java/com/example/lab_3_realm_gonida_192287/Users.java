4cdc8a3bcb8b6b898c92778235abf23c3c600dc1
Osteoidwinter07 <robertojoaquin2001@gmail.com>
2021-08-05 20:53:22 +0800

    Added Users.java

---
+++ Final_Project-Eat@Home/app/src/main/java/com/example/lab_3_realm_gonida_192287/Users.java
@@ -0,0 +1,10 @@
+package com.example.lab_3_realm_gonida_192287;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Users  extends RealmObject {
+
+    @PrimaryKey
+
+}
